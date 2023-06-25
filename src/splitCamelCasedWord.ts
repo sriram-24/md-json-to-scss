c9472398679b4307e0730b4ed491fb7cffff550e
sriram-24 <sriram1999m@gmail.com>
2023-06-25 19:03:20 +0530

    regex issue fix.

--- src/splitCamelCasedWord.ts
+++ src/splitCamelCasedWord.ts
@@ -1,13 +1,12 @@
 export const splitCamelCaseWord = (word : string) : string => {
-    let newword : string = '';
-    const regex : string='/(?=[A-Z])/'
-    word.split(regex).map((s : string) => {
-      if(newword==''){
-        newword+=s.toLowerCase()
-      }
-      else{
-        newword+='-'+s.toLowerCase()
-      }
-    });
-    return newword
-  }
+  let newword : string = '';
+  word.split(/(?=[A-Z])/).map((s : string) => {
+    if(newword==''){
+      newword+=s.toLowerCase()
+    }
+    else{
+      newword+='-'+s.toLowerCase()
+    }
+  });
+  return newword
+}
